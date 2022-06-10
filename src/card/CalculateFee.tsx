b0d53ae553077b3d2bc3a6774933a66cd5d1096e
Evgeny Rozanov <e.rosanov@gmail.com>
2022-06-10 23:10:32 +0400

    calculate fee

--- src/card/CalculateFee.tsx
+++ src/card/CalculateFee.tsx
@@ -7,10 +7,13 @@ import {
   Center,
   Input,
   Divider,
+  HStack,
+  CloseButton
 } from "@chakra-ui/react"
 
 import { Pages } from "./pages"
 import { useStore } from "../store/useStore"
+import { ArrowBackIcon } from "@chakra-ui/icons"
 
 type CalculateFeeProps = {
 }
@@ -19,21 +22,29 @@ export const CalculateFee: FC<CalculateFeeProps> = (props) => {
   const { page, accSymbol } = useStore();
   if (page === Pages.CALCULATE_FEE) {
     return (
-      <Box padding='10'>
-        <VStack borderRadius='15px'>
-          <Box mb={5} fontSize={16}>
+      <Box>
+        <HStack p={2} width="100%">
+          <ArrowBackIcon mr="100px"/>
+          <Center mb={5} fontSize={16}>
               Amount & Fees
-          </Box>
-          <Box mb={5} fontSize={14} textAlign='left' width='95%'>
-              Send
-          </Box>
-          <Input fontSize= {14} borderRadius='15px' size='lg' placeholder={`How much ${accSymbol} will you send?`}/>
-          <Box pt={2} mb={5} fontSize={14} textAlign='left' width='95%'>
-              Receiving
-          </Box>
-          <Input fontSize= {14} borderRadius='15px' size='lg' />
-          <Divider my='20px'/>
-        </VStack>
+          </Center>
+        </HStack>
+        <Box pt = {5} pl={10} pr={10} pb={5}>
+          <VStack borderRadius='15px'>
+            <HStack spacing='24px' borderRadius={14} width="100%" borderWidth={1}>
+              <Input fontSize= {14} borderRadius={14} border={0} size='lg' placeholder={`How much ${accSymbol} will you send?`}/>
+              <Center fontSize={13} w='90px' h='45px'>
+                  {accSymbol}
+              </Center>
+            </HStack>
+            <HStack spacing='24px' borderRadius={14} width="100%" borderWidth={1}>
+              <Center fontSize={13} w='90px' h='45px'>
+                Receiving
+              </Center>
+            </HStack>
+          </VStack>
+        </Box>
+        <Divider mt={20} my='20px'/>
       </Box>
     )
   } else {
