e52bdf95a9ac79d73d4df71c2b9f6ee6a14725d0
Evgeny Rozanov <e.rosanov@gmail.com>
2022-06-10 14:45:03 +0400

    calculate fee page

--- src/card/CalculateFee.tsx
+++ src/card/CalculateFee.tsx
@@ -4,6 +4,7 @@ import {
   Box,
   VStack,
   Select,
+  Center,
   Input,
   Divider,
 } from "@chakra-ui/react"
@@ -18,17 +19,21 @@ export const CalculateFee: FC<CalculateFeeProps> = (props) => {
   const { page, accSymbol } = useStore();
   if (page === Pages.CALCULATE_FEE) {
     return (
-      <Box padding='6'>
-            <VStack borderRadius='15px'>
-              <Box mb={5} fontSize={16}>
-                  Amount & Fees
-              </Box>
-              <Input fontSize= {14} borderRadius='15px' size='lg' placeholder={`How much ${accSymbol} will you send?`}/>
-              <Select fontSize= {14} borderRadius='15px' size='lg'>
-                <option value='eth'>Ethereum</option>
-              </Select>
-              <Divider my='20px'/>
-            </VStack>
+      <Box padding='10'>
+        <VStack borderRadius='15px'>
+          <Box mb={5} fontSize={16}>
+              Amount & Fees
+          </Box>
+          <Box mb={5} fontSize={14} textAlign='left' width='95%'>
+              Send
+          </Box>
+          <Input fontSize= {14} borderRadius='15px' size='lg' placeholder={`How much ${accSymbol} will you send?`}/>
+          <Box pt={2} mb={5} fontSize={14} textAlign='left' width='95%'>
+              Receiving
+          </Box>
+          <Input fontSize= {14} borderRadius='15px' size='lg' />
+          <Divider my='20px'/>
+        </VStack>
       </Box>
     )
   } else {
