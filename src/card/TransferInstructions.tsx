b85e4df2b95694adbfe09c93f50f335997d860c7
Evgeny Rozanov <e.rosanov@gmail.com>
2022-06-13 13:37:21 +0400

    copy button

--- src/card/TransferInstructions.tsx
+++ src/card/TransferInstructions.tsx
@@ -1,24 +1,61 @@
-import { ArrowBackIcon } from "@chakra-ui/icons"
-import { Box, Center, HStack } from "@chakra-ui/react"
-import { FC } from "react"
+import { ArrowBackIcon, CopyIcon } from "@chakra-ui/icons"
+import { Box, Center, HStack, Popover, PopoverBody, PopoverContent, PopoverTrigger, Portal, Tooltip, VStack } from "@chakra-ui/react"
+import { FC, useState } from "react"
 
 import { CALCULATE_FEE_STEP } from "../store/actions"
 import { useStore } from "../store/useStore"
-
+import { config } from '../config/config'
 
 type Props =  {
 }
-export const TransferInstructions: FC<Props> = (props) => {
-  const {  dispatch } = useStore();
 
+type CopyPopoverProps = {
+  address: string
+}
+
+export const CopyPopover: FC<CopyPopoverProps> = (props) => {
+  const [open, setOpen]= useState(false)
+  const onOpen = () => {
+    setOpen(true)
+    navigator.clipboard.writeText(props.address)
+    setTimeout(() => { setOpen(false)}, 400)  
+  }
   return (
-    <Box>
+    <Popover placement='right-start' onOpen={onOpen} isOpen = {open}>
+      <PopoverTrigger>
+      <CopyIcon ml={1}/>
+      </PopoverTrigger>
+      <Portal>
+        <PopoverContent maxWidth={14}>
+          Copied
+        </PopoverContent>
+      </Portal>
+    </Popover>
+  )
+}
+export const TransferInstructions: FC<Props> = (props) => {
+  const { accSymbol, dispatch } = useStore()
+  const accDepositAddress =  config.tokens.filter(token => token.accSymbol === accSymbol)[0].accDepositAddress
+  return (
+    <Box fontSize={16}>
       <HStack p={2} width="100%">
-        <ArrowBackIcon mr="100px" onClick={()=> dispatch({type: CALCULATE_FEE_STEP})}/>
+        <ArrowBackIcon mr="90px" onClick={()=> dispatch({type: CALCULATE_FEE_STEP})}/>
         <Center mb={5} fontSize={16}>
             Transfer Instructions
         </Center>
       </HStack>
+      <Box pt = {5} pl={10} pr={10} pb={5}>
+        <VStack>
+        <Box>Send { accSymbol } to </Box>    
+        <Box>
+          [{accDepositAddress}] 
+          <CopyPopover address={accDepositAddress}/>
+        </Box>
+        <Box>with <b>memo</b> </Box> 
+        <Box>(txs without memo will be lost)</Box>
+        </VStack>
+         
+      </Box>
     </Box>
   )
 }
