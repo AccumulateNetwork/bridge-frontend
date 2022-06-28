6dc3e9b1b453dac5ae8e780ea6f91ce87618745f
Evgeny Rozanov <e.rosanov@gmail.com>
2022-06-28 19:51:49 +0400

    refactoring

--- src/Main.tsx
+++ src/Main.tsx
@@ -10,32 +10,31 @@ type Props = {}
 export const Main: FC<Props> = () => {
     const { account, chainId } = useWeb3React()
     return (
-        <VStack mt={-1} pt={1} minH='calc(93vh - 107px)'>
-            {account && chainId !== config.evmNetwork.chainId ? 
-                (
-                    <Alert  justifyContent='center'
-                     status='error'>
-                    <AlertIcon />
-                    Wrong network. Use chain with id {config.evmNetwork.chainId} 
-                  </Alert>
+      <VStack mt={-1} pt={1} minH='calc(93vh - 107px)'>
+        {account && chainId !== config.evmNetwork.chainId ? 
+        (
+          <Alert  justifyContent='center' status='error'>
+              <AlertIcon />
+              Wrong network. Use chain with id {config.evmNetwork.chainId} 
+          </Alert>
                 
-                ) : (
-                <Routes>
-                <Route path="/" element={<Navigate to="mint"/>}/>
-                <Route path={ config.tab1Path } element={ <Card tabIndex={0}/>}/>
-                <Route path={ config.tab2Path } element={ <Card tabIndex={1}/>}/>
-                <Route
-                path="*"
-                element={
-                    <div>
-                    <h2>404 Page not found</h2>
-                    </div>
-                }
-                />
-                </Routes>
-              
-                )
-            }  
-        </VStack>    
+        ) : 
+        (
+          <Routes>
+            <Route path="/" element={<Navigate to="mint"/>}/>
+            <Route path={ config.tab1Path } element={ <Card tabIndex={0}/>}/>
+            <Route path={ config.tab2Path } element={ <Card tabIndex={1}/>}/>
+            <Route
+              path="*"
+              element={
+                <div>
+                  <h2>404 Page not found</h2>
+                </div>
+              }
+            />
+          </Routes>      
+        )
+        }  
+      </VStack>    
     )
 }
