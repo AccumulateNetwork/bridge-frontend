b19ff0050238f08aa7860db2a6577aae610eb35b
ilzheev <a@lovedesign.ru>
2022-07-10 13:50:33 +0200

    update supported chains list

--- src/connectors.ts
+++ src/connectors.ts
@@ -1,9 +1,8 @@
 import { InjectedConnector } from "@web3-react/injected-connector"
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector"
 
-
 const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42]
+  supportedChainIds: [1, 4]
 })
 
 const walletConnect = new WalletConnectConnector({
