bb283582d3bf7fdf0801e284a840bde0ae6c72f9
Evgeny Rozanov <e.rosanov@gmail.com>
2022-09-01 03:03:13 +0400

    refactoring

--- src/chains.ts
+++ src/chains.ts
@@ -1,9 +1,8 @@
-
-  export const Chains = new Map<number, string>([
+export const Chains = new Map<number, string>([
     [1, "Ethereum Main Network"],
     [3, 'Ropsten Test Network'],
     [4,'Rinkeby Test Network'],
     [5, 'Goerli Test Network'],
     [42, 'Kovan Test Network' ],
     [137, 'Polygon Main Network']
-  ])
+])
