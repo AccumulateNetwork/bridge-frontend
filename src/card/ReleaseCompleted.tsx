532d3906db8a288792f041f80ef57e339dc7032a
ilzheev <a@lovedesign.ru>
2022-09-16 09:39:34 +0200

    UI update

--- src/card/ReleaseCompleted.tsx
+++ src/card/ReleaseCompleted.tsx
@@ -3,10 +3,15 @@ import {
   VStack,
   Heading,
   Divider,
-  Button
+  Button,
+  Alert,
+  Tooltip,
+  Text
 } from "@chakra-ui/react"
 
-import { FC } from "react"
+import { CopyIcon } from "@chakra-ui/icons"
+
+import { FC, useState } from "react"
 
 import { useNavigate } from "react-router-dom"
 
@@ -15,18 +20,49 @@ type Props =  {
   transactionHash: string
 }
 
+type CopyTooltipProps = {
+  content: string
+}
+
+export const CopyTooltip: FC<CopyTooltipProps> = (props) => {
+  const [clicked, setClicked]= useState(false)
+  const onClick = () => {
+    setClicked(true)
+    navigator.clipboard.writeText(props.content)
+    setTimeout(() => { setClicked(false) }, 1000)  
+  }
+  return (
+    <Tooltip label={clicked ? "Copied!" : "Copy"} placement="right">
+      <CopyIcon ml={1} onClick={onClick}/>
+    </Tooltip>
+  )
+}
+
 export const ReleaseCompleted: FC<Props> = (props) => {
   const navigate = useNavigate()
   return (
     <Box fontSize={16}>
       <Box pt={5} pl={10} pr={10} pb={5}>
         <VStack>
-          <Heading size='md'>Burn TX Created</Heading>
-          <Divider pt={2} /> 
+          <Heading size='md'>Transaction Sent</Heading>
+          <Divider pt={2} />
           <Box py={2}>
-            When your tx with hash <Heading size="sm"> { props.transactionHash }</Heading> is confirmed by the network, 
-            Accumulate Bridge nodes will catch it up and send { props.symbol } to your address. 
-            This operation usually takes up to 5 minutes.
+            Transaction hash:
+          </Box>    
+          <Alert status='warning' justifyContent='center' display='block'>
+            <strong>{ props.transactionHash }</strong>
+            <CopyTooltip content={ props.transactionHash }/>
+          </Alert>
+          <Box py={2}>
+            <Text fontSize='sm'>
+            When your tx is confirmed by the network, 
+            Accumulate Bridge will catch it up and send <strong>{ props.symbol }</strong> to your address. 
+            </Text>
+          </Box>    
+          <Box py={2}>
+            <Text fontSize='sm'>
+            This operation usually takes up to 5 minutes (after confirmation of your tx).
+            </Text>
           </Box>    
           <Divider pt={4} />
           <Box py={2}>
